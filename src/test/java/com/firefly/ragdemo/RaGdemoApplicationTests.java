152e0eb3b7dc511173ae28a0c0cea9a54fcbeae9
Jcz <firefly550w@icloud.com>
2025-09-17 14:37:59 +0800

    init

---
+++ src/test/java/com/firefly/ragdemo/RaGdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.firefly.ragdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaGdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
